45bc1fd32f80e25f7ae7f1bdc4d6d71904a2645b
HHH <237106102@qq.com>
2021-10-04 23:11:58 +0800

    完成好友排行榜里可收集状态与时间剩余状态的区分

--- EnergyBall.js
+++ EnergyBall.js
@@ -3,10 +3,10 @@ let SysConstant = require('./SysConstant.js');
 function EnergyBall() {
 }
 
-EnergyBall.MAX_RADIUS = SysConstant.DEVICE_WIDETH / 8 / 2
-EnergyBall.MIN_RADIUS = SysConstant.DEVICE_WIDETH / 10 / 2
-EnergyBall.REGION_Y = SysConstant.DEVICE_HEIGHT * 0.2
-EnergyBall.REGION_H = SysConstant.DEVICE_HEIGHT * 0.3
+EnergyBall.MAX_RADIUS = SysConstant.DEVICE_WIDETH / 8 / 2;
+EnergyBall.MIN_RADIUS = SysConstant.DEVICE_WIDETH / 10 / 2;
+EnergyBall.REGION_Y = SysConstant.DEVICE_HEIGHT * 0.2;
+EnergyBall.REGION_H = SysConstant.DEVICE_HEIGHT * 0.3;
 
 /**
  * 找到能量球
@@ -32,6 +32,9 @@ EnergyBall.findEnergyBall = function () {
     // 回收图片
     gray.recycle();
 
+    //还原能量球的Y轴数值
+    energyBallArr.forEach((energyBall) => energyBall.y += EnergyBall.REGION_Y)
+
     return energyBallArr;
 }
 
@@ -42,9 +45,7 @@ EnergyBall.collectEnergyBall = function () {
     let energyBallArr = EnergyBall.findEnergyBall();
 
     //收集能量
-    energyBallArr.forEach(eneryBall => {
-        click(eneryBall.x, eneryBall.y + EnergyBall.REGION_Y)
-    });
+    energyBallArr.forEach(eneryBall => click(eneryBall.x, eneryBall.y));
 }
 
 /**
@@ -63,31 +64,21 @@ EnergyBall.traversalFriendRanking = function () {
         for (let index = indexTemp; index < friendList.length; index++) {
             indexTemp = index;
 
+            let friend = friendList[index];
+
             //处于屏幕底端时向下滚动屏幕
-            if (friendList[index].bounds().bottom == SysConstant.DEVICE_HEIGHT) {
+            if (friend.bounds().bottom == SysConstant.DEVICE_HEIGHT) {
                 rankingList.scrollDown();
                 sleep(1000);
                 break;
             }
 
-            let energyWeight = friendList[index].findOne(textEndsWith("g"))
+            let energyBall = judgingCollectable(cs, friend);
 
-            if (energyWeight) {
-                //判断当前项是否有能量收集
-                let energyWeightBounds = energyWeight.bounds()
-                let collectable = findColorEquals(
-                    cs, -14835603,
-                    energyWeightBounds.left,
-                    energyWeightBounds.top,
-                    energyWeightBounds.right - energyWeightBounds.left,
-                    energyWeightBounds.bottom - energyWeightBounds.top
-                );
-
+            if (energyBall.collectable && !energyBall.remainingTime) {
                 //收集朋友能量
-                if (collectable) {
-                    EnergyBall.enterFriendHomepage(friendList[index]);
-                    cs = captureScreen();
-                }
+                EnergyBall.enterFriendHomepage(friend);
+                cs = captureScreen();
             }
         }
 
@@ -97,17 +88,60 @@ EnergyBall.traversalFriendRanking = function () {
     }
 }
 
+function judgingCollectable(cs, friend) {
+    //找收集标识
+    let fb = friend.bounds();
+    let collectableLogo = null;
+    friend.children().forEach((view) => {
+        if (fb.top == view.bounds().top && fb.right == view.bounds().right) {
+            collectableLogo = view;
+        }
+    })
+
+    //收集标识不存在，类似需邀请的好友
+    if (!collectableLogo) {
+        return { collectable: false };
+    } else {
+        //判断是否有能量收集
+        let collectableLogoBounds = collectableLogo.bounds();
+        let logo = {
+            x: collectableLogoBounds.left,
+            y: collectableLogoBounds.top,
+            weight: collectableLogoBounds.right - collectableLogoBounds.left,
+            height: collectableLogoBounds.bottom - collectableLogoBounds.top
+        }
+
+        let point = findColorEquals(cs, -14835603, logo.x, logo.y, logo.weight, logo.height);
+
+        if (!point) {
+            return { collectable: false };
+        } else {
+            let remainingTime = collectableLogo.text()
+
+            if (remainingTime == "") {
+                return { collectable: true, remainingTime: null };
+            } else {
+                //返回可收集能量的剩余时间
+                return { collectable: true, remainingTime: parseInt(remainingTime) };
+            }
+        }
+    }
+}
+
 /**
  * 进入好友主页
  * @param {} friend 
  */
 EnergyBall.enterFriendHomepage = function (friend) {
+    //进入好友主页
     friend.clickCenter();
     text("点击展开好友动态").waitFor();
     sleep(500)
 
+    //收取能量
     EnergyBall.collectEnergyBall();
 
+    //返回排行榜
     back();
     text("排行榜").waitFor();
     sleep(500)
